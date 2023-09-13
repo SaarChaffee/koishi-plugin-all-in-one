b76b4bcde1c6e87a02658ed02a153e2bf4bd99cb
SaarChaffee <saarchaffee@qq.com>
2023-09-13 23:00:30 +0800

    feat: merge text

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema, Service } from 'koishi'
+import { } from '@koishijs/assets'
 
-export const name = 'all-in-one'
+const logger = new Logger('all-in-one')
 
-export interface Config {}
+declare module 'koishi' {
+  interface Context {
+    allInOne: AllInOne
+  }
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  urls: string[]
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  urls: Schema.array(Schema.string()).default([]).description('tracker list')
+})
+
+class AllInOne extends Service {
+  private config: Config
+
+  constructor(ctx: Context, config: Config) {
+    super(ctx, 'allInOne', true)
+    this.config = config
+  }
+
+  async get(): Promise<string> {
+    let promises: Promise<string>[] = []
+
+    for (const url of this.config.urls) {
+      promises.push(this.ctx.http.get(url))
+    }
+
+    return Array.from(new Set(
+      (await Promise.all(promises))
+        .map((resp) => resp.split(/\n\n|\n/).filter((str: string) => str.length > 0)).flat())
+    ).join('\n')
+  }
+}
+
+export function apply(ctx: Context, config: Config) {
+  ctx.plugin(AllInOne, config)
+
+  ctx.router.get('/trackerlist', async (cc, next) => {
+    const resp = await ctx.allInOne.get()
+    if (resp.length > 0) {
+      cc.status = 200
+      cc.type = 'text'
+      return cc.body = resp
+    } else {
+      cc.status = 500
+      cc.type = 'text'
+      return cc.body = 'error'
+    }
+  })
 }
